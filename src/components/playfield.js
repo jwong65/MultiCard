31198226d1d53838a7c2b46c5d4803ca8496dc2d
jwong65 <jason.wong6507@gmail.com>
2023-06-08 17:06:41 -0400

    Added sass files and tested for the regular positioning

--- src/components/playfield.js
+++ src/components/playfield.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { useState } from 'react'
+import "../css/form.css"
 
 export default function Playfield() {
     const [state, changeState]= useState(1)
   return (
     <>
-        <div>
+        <div className='play'>
             playfield testing : {state}
         </div>
-        <button onClick={()=> changeState(state+1)}>This button is changes the state.</button>
+        <button className='testbutton' onClick={()=> changeState(state+1)}>This button is changes the state.</button>
     </>
   )
 }
