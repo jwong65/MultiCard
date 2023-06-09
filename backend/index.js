30f045d2c0f45ecbac2dd33008d828108886c677
jwong65 <jason.wong6507@gmail.com>
2023-06-09 18:41:44 -0400

    Testing the post message using examples

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,38 @@ import express from 'express';
 import {v4 as uuidv4 } from 'uuid'
 const app = express();
 
+// Middleware to extract json and make is accessible
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+let messages = {
+    1: {
+      id: '1',
+      text: 'Hello World',
+      userId: '1',
+    },
+    2: {
+      id: '2',
+      text: 'By World',
+      userId: '2',
+    },
+  };
+  
+  
 
 app.get('/users', (req, res)=>{
     return res.send('Recieved a GET')
 })
 
 app.post('/users', (req, res)=>{
-    return res.send('Recievved a POST')
+    const id =uuidv4();
+    const message ={
+        id,
+        text: req.body.text,
+    };
+    messages[id] = message;
+    return res.send(message)
 })
 
 app.put('/users/:userId', (req, res)=>{
